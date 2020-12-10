f9d74dd99180096b8adacaf6a0c3f329b426f9c9
Sarah Leong <sleong20@stuy.edu>
2020-12-10 13:20:48 -0500

    began modifying the constructor

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -2,18 +2,18 @@ public class NoNullArrayList<T> extends ArrayList<T> {
     //constructors
     //default
     public NoNullArrayList() {
-
+        super();
     }
     //set startingCapacity
     public NoNullArrayList(int startingCapacity) {
-
+        super(startingCapacity);
     }
 
     //methods
-    public void add() {
+    public boolean add(T element) {
 
     }
-    public void add(int index) {
+    public boolean add(int index, T element) {
 
     }
     public void set(int index, T value) {
