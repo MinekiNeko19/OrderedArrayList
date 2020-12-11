610c69b2161afb0a6159bbbccad3d091dfd0092c
Sarah Leong <sleong20@stuy.edu>
2020-12-11 10:13:56 -0500

    worked on constructors; attempted to code set and add;

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class NoNullArrayList<T> extends ArrayList<T> {
     //constructors
     //default
@@ -11,12 +13,37 @@ public class NoNullArrayList<T> extends ArrayList<T> {
 
     //methods
     public boolean add(T element) {
-
+        if (element==null) {
+            throw new IllegalArgumentException();
+        } else {
+            this.add(element);
+        }
     }
     public boolean add(int index, T element) {
-
+        if (element == null) {
+            throw new IllegalArgumentException();
+        } else {
+            this.add(index, element);
+        }
     }
     public void set(int index, T value) {
+        if (value == null) {
+            throw new IllegalArgumentException();
+        } else {
+            this.set(index, value);
+        }
+    }
 
+    public static void main(String[] args) {
+        // NoNullArrayList<int> sequence = new NoNullArrayList();
+        // for (int i = 0; i < 10; i++) {
+        //     sequence.add(i);
+        // }
+        // try {
+        //     sequence.add(null);
+        // } catch (IllegalArgumentException e) {
+        //     e.printStackTrace();
+        //     System.out.println("Cannot add null value to NoNullArrayList");
+        // }
     }
 }
