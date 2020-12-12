36e35bf3a5bd7817b05e4bfed0e6d325c8511515
Sarah Leong <sleong20@stuy.edu>
2020-12-12 10:43:21 -0500

    added and tested set method

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -30,7 +30,11 @@ public class NoNullArrayList<T> extends ArrayList<T> {
     }
 
     public T set(int index, T value) {
-        return value;
+        if (value == null) {
+            throw new IllegalArgumentException();
+        } else {
+            return(super.set(index, value));
+        }
     }
 
     public static void main(String[] args) {
@@ -44,24 +48,30 @@ public class NoNullArrayList<T> extends ArrayList<T> {
 
         // Testing add(element)
         NoNullArrayList<Integer> c = new NoNullArrayList<Integer>();
-        System.out.println(c.add(10));
-        try {
-            c.add(null);
-        } catch (IllegalArgumentException e) {
-            e.printStackTrace();
-            System.out.println("Cannot have null values in NoNullArrayList");
-        }
+        // System.out.println(c.add(10));
+        // try {
+        //     c.add(null);
+        // } catch (IllegalArgumentException e) {
+        //     e.printStackTrace();
+        //     System.out.println("Cannot have null values in NoNullArrayList");
+        // }
         for (int i = 0; i < 15; i++) {
             c.add(5);
         }
 
-        c.add(6, 20);
+        // c.add(6, 20);
+        // System.out.println(c.toString());
+        // try {
+        //     c.add(3, null);
+        // } catch (IllegalArgumentException e) {
+        //     e.printStackTrace();
+        //     System.out.println("Cannot have null values in NoNullArrayList");
+        // }
+
+        // Testing set(index, value)
+        System.out.println(c.set(10,90));
         System.out.println(c.toString());
-        try {
-            c.add(3, null);
-        } catch (IllegalArgumentException e) {
-            e.printStackTrace();
-            System.out.println("Cannot have null values in NoNullArrayList");
-        }
+        System.out.println(c.set(0,80));
+        System.out.println(c.toString());
     }
 }
