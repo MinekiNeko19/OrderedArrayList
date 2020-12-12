f585f9c3f5049e3bbfeee0c61c80b69cd4005d82
Sarah Leong <sleong20@stuy.edu>
2020-12-12 11:51:36 -0500

    added and tested findIndex helper method

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -1,23 +1,47 @@
 public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T> {
     // Constructors
     public OrderedArrayList() {
-
+        super();
     }
 
     public OrderedArrayList(int startingCapacity) {
-
+        super(startingCapacity);
     }
 
     // Methods
-    public boolean add(T element) {
+    public int findIndex(T value) {
+        for (int i = 0; i < size(); i++) {
+            if (value.compareTo(get(i)) <= 0) {
+                return i;
+            }
+        }
+        return size();
+    }
 
+    public boolean add(T element) {
+        if (element == null) {
+            throw new IllegalArgumentException();
+        } else {
+            super.add(findIndex(element), element);
+        }
+        return true;
     }
 
     public void add(int index, T element) {
-
+        add(element);
     }
 
     public T set(int index, T value) {
-        
+        return value;
+    }
+
+    public static void main(String[] args) {
+        OrderedArrayList<Integer> a = new OrderedArrayList<Integer>();
+        for (int i = 0; i < 20; i = i + 2) {
+            a.add(i);
+        }
+        System.out.println(a.toString());
+        System.out.println(a.add(5));
+        System.out.println(a.toString());
     }
 }
